6ad17b081dfe5085bb24d865dbc66b7ad663b9bc
Yuya Isaka <zxwg6329@gmail.com>
2022-04-29 13:39:49 +0900

    fix

--- step2alpha/serial.c
+++ step2alpha/serial.c
@@ -41,13 +41,13 @@
 // SCIの定義
 // 先頭アドレス
 // 構造体でキャスト，　0xffffb0というアドレスをh8_3069f_sciという構造体でキャスト -> メモリマップドIOで周辺コントローラのアドレス経由でアクセスするとき，　アドレスを構造体に無理やりキャストして入れて，操る
-#define SCI0 ((volatile registers *)0xffffb0)
-#define SCI1 ((volatile registers *)0xffffb8) // シリアルコネクタは1
-#define SCI2 ((volatile registers *)0xffffc0)
+#define SCI0 ((volatile sci *)0xffffb0)
+#define SCI1 ((volatile sci *)0xffffb8) // シリアルコネクタは1
+#define SCI2 ((volatile sci *)0xffffc0)
 
 // SCIの各種レジスタの定義
 // SCIはCPUに内蔵されているのでマッピングされているアドレスは決まっている
-typedef struct h8_3069f_sci
+typedef struct _sci
 {
 	volatile uint8 smr; // 0xffffb8, シリアル通信のモード設定
 	volatile uint8 brr; // 0xffffb9, シリアル通信の速度（ボーレート)の設定, ビットレートレジスタ
@@ -56,54 +56,49 @@ typedef struct h8_3069f_sci
 	volatile uint8 ssr; // 0xffffbc, 送信完了/受信完了などを表す
 	volatile uint8 rdr; // 0xffffbd, 受信した１文字を読み出す
 	volatile uint8 scmr;
-} registers;
+} sci;
 
 // SMRの各ビットの定義
 // シリアルモードレジスタ
 // OR論理で結合して設定する
 // 現在は，データ長が８ビット，ストップビット長は１ビット，パリティ無しという設定がシリアル接続の標準
 // SMRは0で問題ない
-#define H8_3069F_SCI_SMR_CKS_PER1 (0 << 0)	// クロックセット（クロックをそのまま利用）
-#define H8_3069F_SCI_SMR_CKS_PER4 (1 << 0)	// クロックセット
-#define H8_3069F_SCI_SMR_CKS_PER16 (2 << 0) // クロックセット
-#define H8_3069F_SCI_SMR_CKS_PER64 (3 << 0) // クロックセット
-#define H8_3069F_SCI_SMR_MP (1 << 2)		//
-#define H8_3069F_SCI_SMR_STOP (1 << 3)		// ストップビット長が１か2
-#define H8_3069F_SCI_SMR_OE (1 << 4)		// 偶数パリティか奇数パリティか
-#define H8_3069F_SCI_SMR_PE (1 << 5)		// パリティ無効か有効
-#define H8_3069F_SCI_SMR_CHR (1 << 6)		// データ長が8か7
-#define H8_3069F_SCI_SMR_CA (1 << 7)		// 調歩動機式モードかクロック同期式モード
+#define SMR_CKS_PER1 (0 << 0)  // クロックセット（クロックをそのまま利用）
+#define SMR_CKS_PER4 (1 << 0)  // クロックセット
+#define SMR_CKS_PER16 (2 << 0) // クロックセット
+#define SMR_CKS_PER64 (3 << 0) // クロックセット
+#define SMR_MP (1 << 2)		   //
+#define SMR_STOP (1 << 3)	   // ストップビット長が１か2
+#define SMR_OE (1 << 4)		   // 偶数パリティか奇数パリティか
+#define SMR_PE (1 << 5)		   // パリティ無効か有効
+#define SMR_CHR (1 << 6)	   // データ長が8か7
+#define SMR_CA (1 << 7)		   // 調歩動機式モードかクロック同期式モード
 
 // SCRの各ビットの定義
 // シリアルコントロールレジスタ
 // シリアル入出力の制御(シリアル送受信と割り込み)
 // OR論理で結合して設定する
-#define H8_3069F_SCI_SCR_CKE0 (1 << 0) // クロックイネーブル
-#define H8_3069F_SCI_SCR_CKE1 (1 << 1) // クロックイネーブル
-#define H8_3069F_SCI_SCR_TEIE (1 << 2) // クロックイネーブル
-#define H8_3069F_SCI_SCR_MPIE (1 << 3) // クロックイネーブル
-#define H8_3069F_SCI_SCR_RE (1 << 4)   // 受信有効
-#define H8_3069F_SCI_SCR_TE (1 << 5)   // 送信有効
-#define H8_3069F_SCI_SCR_RIE (1 << 6)  // 受信割り込み有効
-#define H8_3069F_SCI_SCR_TIE (1 << 7)  // 送信割り込み有効
+#define SCR_CKE0 (1 << 0) // クロックイネーブル
+#define SCR_CKE1 (1 << 1) // クロックイネーブル
+#define SCR_TEIE (1 << 2) // クロックイネーブル
+#define SCR_MPIE (1 << 3) // クロックイネーブル
+#define SCR_RE (1 << 4)	  // 受信有効
+#define SCR_TE (1 << 5)	  // 送信有効
+#define SCR_RIE (1 << 6)  // 受信割り込み有効
+#define SCR_TIE (1 << 7)  // 送信割り込み有効
 
 // SSRの各ビットの定義
 // シリアルステータスレジスタ
-#define H8_3069F_SCI_SSR_MPBT (1 << 0)
-#define H8_3069F_SCI_SSR_MPB (1 << 1)
-#define H8_3069F_SCI_SSR_TEND (1 << 2)
-#define H8_3069F_SCI_SSR_PER (1 << 3)
-#define H8_3069F_SCI_SSR_FERERS (1 << 4)
-#define H8_3069F_SCI_SSR_ORER (1 << 5)
-#define H8_3069F_SCI_SSR_RDRF (1 << 6) // 受信完了
-#define H8_3069F_SCI_SSR_TDRE (1 << 7) // 送信完了
+#define SSR_MPBT (1 << 0)
+#define SSR_MPB (1 << 1)
+#define SSR_TEND (1 << 2)
+#define SSR_PER (1 << 3)
+#define SSR_FERERS (1 << 4)
+#define SSR_ORER (1 << 5)
+#define SSR_RDRF (1 << 6) // 受信完了
+#define SSR_TDRE (1 << 7) // 送信完了
 
-typedef struct _sci
-{
-	volatile registers *s;
-} sci;
-
-static volatile registers *regs[SERIAL_SCI_NUM] = {
+static volatile sci *regs[SERIAL_SCI_NUM] = {
 	SCI0,
 	SCI1,
 	SCI2,
@@ -132,7 +127,7 @@ static volatile registers *regs[SERIAL_SCI_NUM] = {
 // デバイス初期化
 int serial_init(int index)
 {
-	volatile registers *s = regs[index];
+	volatile sci *s = regs[index];
 
 	// 1. シリアル送受信と割り込みを全て無効化
 	s->scr = 0;
@@ -140,7 +135,7 @@ int serial_init(int index)
 	s->smr = 0;
 	s->brr = 64; // 20MHzのクロックから9600bpsを生成（水晶発信器を入れ替え25MHzにした場合は80） 1秒間に９６００ビットを転送できる速度, ドキュメントに設定値が書いてある，　SCIのボーレートはSCIに供給されるクロックによて決まる（クロック周波数，分周比）
 	// 3. シリアル送受信と割り込みを設定
-	s->scr = H8_3069F_SCI_SCR_RE | H8_3069F_SCI_SCR_TE; // 受信有効，　送信有効
+	s->scr = SCR_RE | SCR_TE; // 受信有効，　送信有効
 	s->ssr = 0;
 
 	return 0;
@@ -149,9 +144,9 @@ int serial_init(int index)
 // 送信可能か？
 int serial_is_send_enable(int index)
 {
-	volatile registers *s = regs[index];
+	volatile sci *s = regs[index];
 	// SSRの送信完了ビットを監視
-	return (s->ssr & H8_3069F_SCI_SSR_TDRE);
+	return (s->ssr & SSR_TDRE);
 }
 
 // 1文字送信
@@ -165,7 +160,7 @@ int serial_is_send_enable(int index)
 // シリアルへの文字出力関数(端末変換を行わない)
 int serial_send_byte(int index, unsigned char c)
 {
-	volatile registers *s = regs[index];
+	volatile sci *s = regs[index];
 
 	// 1. 送信可能になるまで待つ(ビジーループ)
 	while (!serial_is_send_enable(index))
@@ -173,7 +168,7 @@ int serial_send_byte(int index, unsigned char c)
 	// 2. 書き込みたい文字を書き込む
 	s->tdr = c;
 	// 3. SSRの送信完了ビットを落とす(落とすことで送信開始を依頼, 0111111のようなマスクを生成して&=すると7ビット目だけ0にできる)
-	s->ssr &= ~H8_3069F_SCI_SSR_TDRE; // 送信開始
+	s->ssr &= ~SSR_TDRE; // 送信開始
 	// 4. 送信完了すると，自動的に送信完了ビット(sci->ssr[7]が立てられる（H８マイコンのシリアルコントローラの設定） ... 自分で作るときもこういう設計にしてドキュメントにまとめる必要があるな
 
 	return 0;
