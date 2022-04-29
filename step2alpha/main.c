2f29743a07b985192d9643e0dc1e3925a28528cb
Yuya Isaka <zxwg6329@gmail.com>
2022-04-29 13:23:52 +0900

    fix

---
+++ step2alpha/main.c
@@ -0,0 +1,34 @@
+// main()関数
+#include "defines.h"
+#include "serial.h"
+#include "lib.h"
+
+// ファームウェア生成
+int main(void)
+{
+	// SCIのレジスタをデータ長を8ビット，ストップビット長を1,パリティ無しで初期化
+	// OSがインタフェースを用意する = デバイスドライバを書く
+	serial_init(SERIAL_DEFAULT_DEVICE);
+
+	puts("Hello World!\n");
+
+	putxval(0x10, 0);
+	puts("\n");
+	putxval(0xffff, 0);
+	puts("\n");
+	putxval(3, 0);
+	puts("\n");
+	putxval(100, 0);
+	puts("\n");
+	putxval('a', 0);
+	puts("\n");
+	putxval('a', 2);
+	puts("\n");
+	putxval('b', 0);
+	puts("\n");
+
+	while (1)
+		;
+
+	return 0;
+}
